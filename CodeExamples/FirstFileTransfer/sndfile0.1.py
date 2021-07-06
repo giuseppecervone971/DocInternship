a348685a752629ea35e8a52f2cf6550c1bda86af
giuseppecervone971 <giuseppe.cervone.971@gmail.com>
2021-07-06 11:27:48 +0200

    Primo prototipo file transfer, con time counter

--- CodeExamples/FirstFileTransfer/sndfile0.1.py
+++ CodeExamples/FirstFileTransfer/sndfile0.1.py
@@ -1,4 +1,7 @@
 import serial
+import time
+import os
+from hurry.filesize import size,alternative
 
 ser = serial.Serial(
         port = "/dev/serial0",
@@ -8,9 +11,15 @@ ser = serial.Serial(
         bytesize = serial.EIGHTBITS,
         timeout=1
         )
+#Initialize serial port
+
+filesize = size(os.path.getsize('log.txt'), system=alternative) #gets filesize in a nice manner to read, will be used for performance purposes
+
+print("Starting transfer... Time started")
+start_time= time.time()
+#Here we open the file, and keep on reading until there's lines, we then send <<EOF>> to signal to the other machine that file transfer is over.
 
 f = open("log.txt","rb")
-
 line = f.readline
 while line:
     ser.write(line)
@@ -20,3 +29,6 @@ ser.write(b"<<EOF>>")
 
 f.close()
 
+end time = time.time() - start_time
+
+print('Transfer completed... Filesize:',filesize,'. Time:',end_time,'seconds.')
