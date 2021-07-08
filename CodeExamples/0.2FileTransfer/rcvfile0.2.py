0cf009082831f1e81eb5bc328f48eb44c591d28c
giuseppecervone971 <47979877+giuseppecervone971@users.noreply.github.com>
2021-07-08 22:31:12 +0200

    Update rcvfile0.2.py

--- CodeExamples/0.2FileTransfer/rcvfile0.2.py
+++ CodeExamples/0.2FileTransfer/rcvfile0.2.py
@@ -20,12 +20,11 @@ sha256_hash = hashlib.sha256() #we will calculate the SHA256 of the file to veri
 f = open("log.txt", "wb") #open file to write into
 
 x = ser.readline()
-
-
 while x != eof: #until file comes, decode, if x=eof then check if file transfered correctly
     sha256_hash.update(x) #decoding is done in bytes, so we make sha before making it in bits
     f.write(x)
     x = ser.readline()
+
 f.close()
 
 sha256_send = ser.readline()
